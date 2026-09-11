19494beacf9299c767269ffd580a3d1900759572
Emmanuel Elias <204698737+emma-elias@users.noreply.github.com>
2026-09-11 19:27:54 +0100

    changed path of logo

--- pages/2_Learn.py
+++ pages/2_Learn.py
@@ -9,7 +9,7 @@ from pathlib import Path
 st.set_page_config(page_title="TORSILAB\Learn", layout="wide")
 
 #Define your local image path
-image_path = Path(r"C:\Users\elias\my_python_project\logo.png")
+image_path = Path("logo.png")
 
 #Read the image and encode it to base64
 try:
