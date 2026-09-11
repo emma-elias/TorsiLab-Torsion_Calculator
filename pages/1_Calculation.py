75f8f284260d72a3b1ba11094fe4e09d35bff783
Emmanuel Elias <204698737+emma-elias@users.noreply.github.com>
2026-09-11 19:26:51 +0100

    path of logo changed

--- pages/1_Calculation.py
+++ pages/1_Calculation.py
@@ -11,7 +11,7 @@ from pathlib import Path
 st.set_page_config(page_title="TORSILAB\Calculation", layout="wide")
 
 #Define your local image path
-image_path = Path(r"C:\Users\elias\my_python_project\logo.png")
+image_path = Path("logo.png")
 
 #Read the image and encode it to base64
 try:
